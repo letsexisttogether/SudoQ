f234478f10d277ad44f00450e409dc4f2f518c7f
letsexisttogether <letmeexist95@gmail.com>
2024-07-25 23:20:28 +0300

    Removed excess MenuItem extra classes

--- Source/Menu/Item/Equal/MenuItemEqual.cpp
+++
@@ -1,8 +0,0 @@
-#include "MenuItemEqual.hpp"
-
-
-bool MenuItemEqual::operator () (const MenuItem& first,
-    const MenuItem& second) const noexcept
-{
-    return first.GetIndex() == second.GetIndex();
-}
--- Source/Menu/Item/Hash/MenuItemHash.cpp
+++
@@ -1,7 +0,0 @@
-#include "MenuItemHash.hpp"
-
-MenuItemHash::Result MenuItemHash::operator () (const MenuItem& item)
-    const noexcept
-{
-    return item.GetIndex();
-}
